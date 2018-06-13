364ebcf5439e3fff50ab7d834d087c13a29269c4
Dyeed <gggssn@qq.com>
2018-06-13 20:24:18 +0800

    admin注册
    Signed-off-by: Dyeed <gggssn@qq.com>

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
-from .models import Post,Comment
+from .models import Post, Comment, Tag, Category
 
-admin.site.register(Post)
+
+class PostAdmin(admin.ModelAdmin):
+    """
+    Post表增加显示字段
+    """
+    list_display = ['title', 'created_date', 'published_date', 'category', 'author']
+
+
+admin.site.register(Post, PostAdmin)
 admin.site.register(Comment)
+admin.site.register(Category)
+admin.site.register(Tag)
